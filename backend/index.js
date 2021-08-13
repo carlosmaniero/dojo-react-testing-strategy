2e76da52d7b847b5e77868b5a38dfff2e8b175c9
Maniero <carlosmaniero@gmail.com>
2021-08-13 06:17:20 -0300

    adds a simple back-end

---
+++ backend/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const fetch = require('node-fetch');
+const cors = require('cors')
+const port = 3000;
+const pokedexDB = 'https://raw.githubusercontent.com/fanzeyi/pokemon.json/master/pokedex.json';
+const app = express();
+app.use(cors());
+
+let responseBody;
+
+app.get('/', async (req, res) => {
+    if (!responseBody) {
+        console.log(`Fetching ${pokedexDB}`)
+        const response = await fetch(pokedexDB);
+        responseBody = await response.json();
+    }
+
+    res.send(responseBody.slice(0, 300).map((pokemon) => ({
+        ...pokemon,
+        sprite: `https://veekun.com/dex/media/pokemon/main-sprites/omegaruby-alphasapphire/${pokemon.id}.png`
+    })));
+});
+
+app.listen(port, () => {
+  console.log(`Pokédex backend listening at http://localhost:${port}`);
+});
