91114734494fe240d4c7c316347e79a1c002be7d
Maniero <carlosmaniero@gmail.com>
2021-08-13 09:56:12 -0300

    adds max and all enabled tests

--- visual-tests/tests/design-system/counter/Counter.spec.js
+++ visual-tests/tests/design-system/counter/Counter.spec.js
@@ -3,7 +3,19 @@ import {goToComponent, componentToMatchSnapshot} from '../../../storybook';
 
 describe('Counter', () => {
     it('renders min disabled properly', async () => {
-        await goToComponent('design-system-counter-counter--min-disabled', async (page, document) => {
+        await goToComponent('design-system-counter-counter--min-disabled', async (page) => {
+            await componentToMatchSnapshot(page);
+        });
+    });
+
+    it('renders all enabled properly', async () => {
+        await goToComponent('design-system-counter-counter--all-enabled', async (page) => {
+            await componentToMatchSnapshot(page);
+        });
+    });
+
+    it('renders max disabled properly', async () => {
+        await goToComponent('design-system-counter-counter--max-disabled', async (page) => {
             await componentToMatchSnapshot(page);
         });
     });
